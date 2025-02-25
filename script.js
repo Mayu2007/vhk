06997dcc5eccee01eada49ca799fa0a110216e0e
Mayu <dangthithuy2712@gmail.com>
2025-02-25 10:59:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function login()
+{
+ let u = username.value
+ let p = password.value
+ if (u=="123" && p=="1234")
+ {
+   window.open()
+  document.getElementById(compiler).style.display="block"
+ }
+else
+   document.write("sai ten dang nhap hoac mat khau").
